dd6b12e12380a90735459b23dc7961479dbeb7f0
root <root@vultr.guest>
2017-02-16 10:44:10 +0000

    change the images path

--- app/router/update.js
+++ app/router/update.js
@@ -4,9 +4,9 @@ var path = require('path');
 module.exports = function(router, koaBody) {
   return router.post('/upload', koaBody, function *(next) {
     if (this.request.method === 'POST') {
-      var demoFile = path.join('/usr/www/images', 'demo.jpg');
+      var demoFile = path.join('/usr/www/images/', 'demo.jpg');
       var file    = this.request.body.files.file,
-        demoSrc = 'http://' + path.join(this.req.headers.host, `demo.jpg?${Date.now()}`);
+        demoSrc = 'http://' + path.join(this.req.headers.host, `/images/demo.jpg?${Date.now()}`);
 
       this.body = yield new Promise((resolve, reject) => {
         fs
