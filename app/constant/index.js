c456b919a788830e24e5b5bf986f4f6c34e7a988
桑震斌 <sangzhenbin@bamaying.com>
2017-03-29 19:03:04 +0800

    nothing

--- app/constant/index.js
+++ app/constant/index.js
@@ -3,6 +3,6 @@ module.exports = {
   APP_PORT: process.env.PORT || '8000',
   MYSQL_HOST: process.env.MYSQL_HOST || '127.0.0.1',
   MYSQL_PORT: process.env.MYSQL_PORT || '8010',
-  HOST_URI: 'http://www.sangzb.com/'
+  HOST_URI: 'www.sangzb.com'
 };
 
