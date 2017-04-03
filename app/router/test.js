fce66ad87d7ac740abc9d254656802a1f6de4689
sangzb <sangzb@sangzhenbindeMacBook-Pro.local>
2017-04-03 11:01:54 +0800

    添加json测试

--- app/router/test.js
+++ app/router/test.js
@@ -2,12 +2,14 @@ var mysql = require('../mysql');
 
 module.exports = function(router, koaBody) {
   return router.post('/test', koaBody, function *(next) {
-    this.body = this.request.body;
+    this.body = this.request;
   }).get('/', function *(next) {
     var request = this.request;
     var response = this.response;
     var path = this.path;
     var result = yield mysql.getUser();
     response.body = result;
+  }).get('/test/jsonp', koaBody, function *(){
+    this.body = 'test({"name", "test"})';
   })
 };
